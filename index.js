a8b1b35ca773b4db63fb906515b61e3b285eaf9e
Anthony <anthony.z.roldan@gmail.com>
2023-01-05 13:59:12 -0500

    subida archivos 2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const d = document;
+const d = document,
+      $main = d.querySelector('.contenedor');
+
+const paginas = async (url) =>{
+   try {
+      let respuesta = await fetch(url);
+      let text = await respuesta.text();
+      $main.innerHTML = text;
+
+   } catch (err) {
+      console.log(err)
+   }
+}
+
+
+d.addEventListener('click', e =>{
+   e.preventDefault();
+   if(e.target.matches('.centro a')){
+      paginas(e.target.href);
+   }
+})
